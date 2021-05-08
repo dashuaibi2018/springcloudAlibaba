2f90d9924185e532ae07553a836fdf463448ddca
sj <393834461@qq.com>
2021-05-08 11:17:09 +0800

    1

---
+++ src/test/java/com/sujun/demo/SpringcloudAlibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujun.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAlibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
