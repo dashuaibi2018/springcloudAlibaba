929cc0009929f08a1bc52d5ab63652fb4d52a8d2
sj <393834461@qq.com>
2021-05-10 15:24:46 +0800

    1

--- src/main/java/com/sujun/demo/controller/MainController.java
+++ src/main/java/com/sujun/demo/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.sujun.demo.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.sujun.demo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,4 +18,61 @@ public class MainController {
         String msg = personSrv.getMess();
         return msg;
     }
+
+
+    @GetMapping("/np")
+    public String getnp(){
+        String msg = personSrv.getnp();
+//        String msg1 = personSrv.getnp1();
+        return msg;
+    }
+
+    @GetMapping("/getThread")
+    public String getThreadMessage(){
+
+        String msg = personSrv.getThreadMess();
+        return msg;
+    }
+
+
+    @GetMapping("/get4")
+    @SentinelResource(value = "get4")
+    public String get4(){
+        return "get4";
+    }
+
+    @GetMapping("/get5")
+    @SentinelResource(value = "get5")
+    public String get5(){
+        return "get5";
+    }
+
+    /**
+     * @description: 异常降级
+     * @param
+     * @return: java.lang.String
+     * @author: sj
+     * @time: 2021/5/10 2:30 下午
+     */
+    @GetMapping("/getexc")
+    @SentinelResource(value = "getexc")
+    public String getexc() throws Exception{
+        int i =10/0;
+        return "getexc";
+    }
+
+    /**
+     * @description: 热点限流
+     * @param
+     * @return: java.lang.String
+     * @author: sj
+     * @time: 2021/5/10 2:29 下午
+     */
+    @GetMapping("/getHot")
+    @SentinelResource(value = "getHot")
+    public String getHot(String name ,int age){
+        return "name: "+name +"---age: "+age;
+    }
+
+
 }
--- src/main/java/com/sujun/demo/service/PersonService.java
+++ src/main/java/com/sujun/demo/service/PersonService.java
@@ -7,15 +7,43 @@ import org.springframework.stereotype.Service;
 @Service
 public class PersonService {
 
-    @SentinelResource(value = "np",blockHandler = "failback")
+    @SentinelResource(value = "get",blockHandler = "failback")
     public String getMess() {
-
         return "狗子醒醒";
     }
+
+    @SentinelResource(value = "np")
+    public String getnp() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "np醒醒";
+    }
+
+    @SentinelResource(value = "np1")
+        public String getnp1() {
+
+        return "np1醒醒";
+    }
+
+
+    @SentinelResource(value = "thread")
+    public String getThreadMess() {
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "睡了一会儿，狗子醒醒";
+    }
+
+
 
     public String failback(BlockException e){
 
         return "降级了" + e.getMessage();
     }
-
 }
